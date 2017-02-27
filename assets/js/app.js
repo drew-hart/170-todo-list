e95b1a080d8481eec6f6f39f8e58567c1bdb22c6
Drew Hart <drew@drewhart.com>
2017-02-27 16:35:15 -0600

    grey out completed items and enable delete when X is clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-console.log('app.js loaded...');
+// Check off specific todos by clicking
+$('ul').on('click', 'li', function(){
+  $(this).toggleClass('completed')
+});
+
+// click on X to delete todos
+$('ul').on('click', "span", function(event) {
+  $(this).parent().fadeOut(500, function(){
+      $(this).remove();
+  })
+  // prevent event from bubbling. No parent handlers will be notified
+  event.stopPropagation();
+});
+
